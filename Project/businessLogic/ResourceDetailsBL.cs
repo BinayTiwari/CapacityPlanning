6c75581a1adae8cbca32a4cfb5f18bc83650f2d8
Rahul Chaurasia <35951172+Rahul1712@users.noreply.github.com>
2018-08-02 11:19:43 +0530

    ResourceDetailsBL

---
+++ Project/businessLogic/ResourceDetailsBL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Entity;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace businessLogic
+{
+    public class ResourceDetailsBL
+    {
+        public int Insert(CPT_ResourceDetails demandDetails)
+        {
+            using (CPContext db = new CPContext())
+            {
+                try
+                {
+                    db.CPT_ResourceDetails.Add(demandDetails);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            return 1;
+        }
+    }
+}
