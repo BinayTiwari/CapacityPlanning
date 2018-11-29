c73e5b152c953bff554fd286c28be60fffa136da
Vinay Pandey <vinay.pandey@gridinfocom.com>
2018-11-29 17:48:47 +0530

    ResourceMappingBL

--- Project/businessLogic/ResourceMappingBL.cs
+++ Project/businessLogic/ResourceMappingBL.cs
@@ -10,10 +10,6 @@ namespace businessLogic
 {
     public class ResourceMappingBL
     {
-        //public static void Navigate(CPT_ResourceDemand details,string requestID)
-        //{
-        //    Response.Redirect("Allocate_Resource.aspx?RequestID=" + requestID);
-        //}
         public static void ResourceMapping(Repeater rpt)
         {
             try
@@ -45,6 +41,44 @@ namespace businessLogic
                                       v.StatusName,
                                       p.DateOfCreation,
                                       z.PriorityName,
+                                      w.EmployeetName
+
+                                  }).ToList();
+
+                    rpt.DataSource = query1;
+                    rpt.DataBind();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+        public static void ResourceMappingRoleWise(Repeater rpt,int roleID)
+        {
+            try
+            {
+                //clear here
+                using (CPContext db = new CPContext())
+                {
+                    var query1 = (from p in db.CPT_ResourceDemand
+                                  join q in db.CPT_AccountMaster on p.AccountID equals q.AccountMasterID
+                                  join r in db.CPT_ResourceDetails on p.RequestID equals r.RequestID
+                                  join ct in db.CPT_CityMaster on p.CityID equals ct.CityID
+                                  join t in db.CPT_OpportunityMaster on p.OpportunityID equals t.OpportunityID
+                                  join w in db.CPT_ResourceMaster on p.ResourceRequestBy equals w.EmployeeMasterID
+                                  where p.StatusMasterID == 26 && r.ResourceTypeID == roleID
+                                  orderby p.DateOfCreation descending
+
+                                  select new
+                                  {
+                                      p.RequestID,
+                                      q.AccountName,
+                                      ct.CityName,
+                                      t.OpportunityType,
+                                      p.ProcessName,
+                                      p.DateOfCreation,
                                       w.EmployeetName
 
                                   }).ToList();
